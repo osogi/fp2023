0ea0297cdd6db3114c5970f7581e909578703bc1
Osogi <dragonmail135159@gmail.com>
2023-11-05 01:44:39 +0300

    refactor: divide parser into several parts

---
+++ LLVM_IR/lib/parser/common.ml
@@ -0,0 +1,98 @@
+(** Copyright 2023-2024, Efremov Alexey *)
+
+(** SPDX-License-Identifier: CC0-1.0 *)
+
+open Angstrom
+
+let test_parse p show str =
+  match Angstrom.parse_string ~consume:Consume.Prefix p str with
+  | Result.Error _ -> Format.printf "Error"
+  | Result.Ok ast -> Format.printf "%s" (show ast)
+;;
+
+let whitespace = function
+  | ' ' | '\t' | '\n' | '\r' -> true
+  | _ -> false
+;;
+
+let comment =
+  char ';'
+  *> return ()
+  *> skip_while (function
+    | '\n' -> false
+    | _ -> true)
+;;
+
+let whitespaces = many (skip whitespace <|> comment)
+
+let str_integer =
+  take_while1 (function
+    | '0' .. '9' -> true
+    | _ -> false)
+;;
+
+let parse_integer = str_integer >>| int_of_string
+
+let varname_char = function
+  | 'a' .. 'z' -> true
+  | 'A' .. 'Z' -> true
+  | '.' | '$' | '_' | '-' -> true
+  | _ -> false
+;;
+
+let parse_word =
+  let satisfy_char = function
+    | a when varname_char a -> true
+    | '0' .. '9' -> true
+    | _ -> false
+  in
+  take_while1 satisfy_char
+;;
+
+let word str =
+  parse_word
+  >>= fun parsed -> if parsed = str then return str else fail "Parser error: Wrong word"
+;;
+
+let parse_named_name =
+  lift2
+    (fun first_char last_part -> String.make 1 first_char ^ last_part)
+    (satisfy varname_char)
+    (parse_word <|> return "")
+;;
+
+let parse_unnamed_name =
+  lift2
+    (fun name _ -> name)
+    str_integer
+    (peek_char
+     >>= fun c ->
+     match c with
+     | Some c when varname_char c -> fail "Parser error: deprecated name"
+     | _ -> return ())
+;;
+
+let%expect_test _ =
+  test_parse parse_unnamed_name (fun f -> f) "543554";
+  [%expect {| 543554 |}]
+;;
+
+let%expect_test _ =
+  test_parse parse_unnamed_name (fun f -> f) "543554d";
+  [%expect {| Error |}]
+;;
+
+let%expect_test _ =
+  test_parse parse_unnamed_name (fun f -> f) "54,";
+  [%expect {| 54 |}]
+;;
+
+let parse_name = parse_named_name <|> parse_unnamed_name
+
+let parse_local_variable =
+  whitespaces *> char '%' *> parse_name >>| fun name -> Ast.LocalVar name
+;;
+
+let parse_global_variable =
+  whitespaces *> char '@' *> parse_name >>| fun name -> Ast.GlobalVar name
+;;
