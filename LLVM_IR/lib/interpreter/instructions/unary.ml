177a4b1d4cdd8a9fbd2315a5ab4d26180a416d72
Osogi <dragonmail135159@gmail.com>
2023-12-30 19:52:47 +0300

    feat: add ssacheck

---
+++ LLVM_IR/lib/interpreter/instructions/unary.ml
@@ -0,0 +1,22 @@
+(** Copyright 2023-2024, Efremov Alexey *)
+
+(** SPDX-License-Identifier: CC0-1.0 *)
+
+open State
+open Instructions_common
+(* 
+let ifneg res_var tp value =  let* cnst = (get_const_from_value value) in
+   
+
+
+let execute_unary_operation : Ast.unary_operation -> (state, 'a) t = match op with
+| Ast.Fneg (res_var, tp, value) -> (ifneg res_var tp value)
+
+(* let%expect_test _ =
+  test_parse parse_instruction Ast.show_instruction {| %23 = fneg float 123.0   |};
+  [%expect
+    {|
+    (Unary
+       (Fneg ((LocalVar "23"), TFloat, (FromVariable ((LocalVar "val"), TFloat))
+          ))) |}]
+;; *) *)
