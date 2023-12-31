6a00445bbbb533314bba7b5b7f4e006f42474ce0
Osogi <dragonmail135159@gmail.com>
2023-12-31 17:52:52 +0300

    feat: add unary launch

--- LLVM_IR/lib/interpreter/instructions/unary.ml
+++ LLVM_IR/lib/interpreter/instructions/unary.ml
@@ -1,24 +1,23 @@
 (** Copyright 2023-2024, Efremov Alexey *)
 
 (** SPDX-License-Identifier: CC0-1.0 *)
-
 open State
+
 open CommonInterpInstructions
 
+let real_fneg v = Ast.CFloat (Float.neg v)
 
-(* 
-let ifneg res_var tp value =  let* cnst = (get_const_from_value value) in
-   
+let ifneg var tp value =
+  (match tp with
+  | Ast.TFloat ->
+    let* v = get_const_from_value value >>= is_float in
+    write_var var (real_fneg v)
+  | Ast.TVector (_, Ast.TFloat) ->
+    let* v = get_const_from_value value >>= is_vector is_float in
+    write_var var (Ast.CVector (List.map real_fneg v))
+  | _ -> fail "Fneg work with floats\n")
+;;
 
-
-let execute_unary_operation : Ast.unary_operation -> (state, 'a) t = match op with
-| Ast.Fneg (res_var, tp, value) -> (ifneg res_var tp value)
-
-(* let%expect_test _ =
-  test_parse parse_instruction Ast.show_instruction {| %23 = fneg float 123.0   |};
-  [%expect
-    {|
-    (Unary
-       (Fneg ((LocalVar "23"), TFloat, (FromVariable ((LocalVar "val"), TFloat))
-          ))) |}]
-;; *) *)
+let launch_unary_operation : Ast.unary_operation -> (state, instr_launch_res) t = fun instr->( match instr with
+  | Ast.Fneg (variable, tp, value) -> ifneg variable tp value) *> return None
+;;
